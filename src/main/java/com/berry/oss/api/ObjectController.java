c931769135471d4112e3cf88ae55af916372ff14
berry_cooper <berry_cooepr@163.com>
2019-06-04 15:44:31 +0800

    xx

--- src/main/java/com/berry/oss/api/ObjectController.java
+++ src/main/java/com/berry/oss/api/ObjectController.java
@@ -6,46 +6,46 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Title BucketController
+ * Title ObjectController
  * Description
  * Copyright (c) 2019
  * Company  上海思贤信息技术股份有限公司
  *
  * @author berry_cooper
  * @version 1.0
- * @date 2019/6/4 14:56
+ * @date 2019/6/4 15:34
  */
 @RestController
-@RequestMapping("api/bucket")
-public class BucketController {
+@RequestMapping("api/object")
+public class ObjectController {
 
     @GetMapping("list")
-    @ApiOperation("获取 Bucket 列表")
+    @ApiOperation("获取 Object 列表")
     public Result list(){
         return ResultFactory.wrapper();
     }
 
     @PostMapping("create")
-    @ApiOperation("创建 Bucket")
+    @ApiOperation("创建对象")
     public Result create(){
         return ResultFactory.wrapper();
     }
 
     @GetMapping("detail")
-    @ApiOperation("获取 Bucket 详情")
+    @ApiOperation("获取对象详情")
     public Result detail(){
         return ResultFactory.wrapper();
     }
 
     @DeleteMapping("delete")
-    @ApiOperation("删除 Bucket")
+    @ApiOperation("删除对象")
     public Result delete(){
         return ResultFactory.wrapper();
     }
 
-    @PutMapping("updateBucketAcl")
-    @ApiOperation("更新 Bucket 读写权限")
-    public Result updateBucketAcl(){
+    @PutMapping("updateObjectAcl")
+    @ApiOperation("更新对象读写权限")
+    public Result updateObjectAcl(){
         return ResultFactory.wrapper();
     }
 }
---
+++ src/main/java/com/berry/oss/api/StatisticsController.java
@@ -0,0 +1,19 @@
+package com.berry.oss.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Title StatisticsController
+ * Description
+ * Copyright (c) 2019
+ * Company  上海思贤信息技术股份有限公司
+ *
+ * @author berry_cooper
+ * @version 1.0
+ * @date 2019/6/4 15:43
+ */
+@RestController
+@RequestMapping("统计信息")
+public class StatisticsController {
+}
