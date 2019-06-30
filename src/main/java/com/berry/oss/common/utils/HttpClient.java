ff31d479105f84dff099fc6d7458e93221710158
HiCooper <294237781@qq.com>
2019-06-30 11:22:35 +0800

    update http client
    Signed-off-by: HiCooper <294237781@qq.com>

--- src/main/java/com/berry/oss/common/utils/HttpClient.java
+++ src/main/java/com/berry/oss/common/utils/HttpClient.java
@@ -21,13 +21,53 @@ import java.util.concurrent.TimeUnit;
  */
 public class HttpClient {
 
+    /**
+     * 默认 http 客户端
+     */
     private static final OkHttpClient CLIENT;
+    /**
+     * 连接超时时间 单位秒(默认10s)
+     */
+    private static final int CONNECT_TIMEOUT = 10;
+    /**
+     * 回调超时
+     */
+    private static final int CALL_TIMEOUT = 10;
+    /**
+     * 回复超时时间 单位秒(默认30s)
+     */
+    private static final int READ_TIMEOUT = 30;
+    /**
+     * 底层HTTP库所有的并发执行的请求数量
+     */
+    private static final int DISPATCHER_MAX_REQUESTS = 64;
+    /**
+     * 底层HTTP库对每个独立的Host进行并发请求的数量
+     */
+    private static final int DISPATCHER_MAX_REQUESTS_PER_HOST = 16;
+    /**
+     * 底层HTTP库中复用连接对象的最大空闲数量
+     */
+    private static final int CONNECTION_POOL_MAX_IDLE_COUNT = 32;
+    /**
+     * 底层HTTP库中复用连接对象的回收周期（单位分钟）
+     */
+    private static final int CONNECTION_POOL_MAX_IDLE_MINUTES = 5;
 
     static {
+        Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(DISPATCHER_MAX_REQUESTS);
+        dispatcher.setMaxRequestsPerHost(DISPATCHER_MAX_REQUESTS_PER_HOST);
+
+        ConnectionPool pool = new ConnectionPool(CONNECTION_POOL_MAX_IDLE_COUNT,
+                CONNECTION_POOL_MAX_IDLE_MINUTES, TimeUnit.MINUTES);
+
         CLIENT = new OkHttpClient.Builder()
-                .callTimeout(10, TimeUnit.SECONDS)
-                .connectTimeout(10, TimeUnit.SECONDS)
-                .readTimeout(10, TimeUnit.SECONDS)
+                .callTimeout(CALL_TIMEOUT, TimeUnit.SECONDS)
+                .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+                .dispatcher(dispatcher)
+                .connectionPool(pool)
                 .build();
     }
 
