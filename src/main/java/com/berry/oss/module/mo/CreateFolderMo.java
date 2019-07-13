09a2769da688318a54d667c3cad1ce3626b959dc
HiCooper <294237781@qq.com>
2019-07-13 16:07:42 +0800

    xx
    Signed-off-by: HiCooper <294237781@qq.com>

--- src/main/java/com/berry/oss/module/mo/CreateFolderMo.java
+++ src/main/java/com/berry/oss/module/mo/CreateFolderMo.java
@@ -24,7 +24,7 @@ public class CreateFolderMo {
      * 全路径
      */
     @NotBlank
-    @Length(min = 2, max = 254, message = "长度在2～254之间")
+    @Length(min = 2, max = 254, message = "路径长度在2～254之间")
     @Pattern(regexp = "^[^/]((?!//)[a-zA-Z0-9/\\u4E00-\\u9FA5]+)*[^/]$", message = "路径不符合要求")
     private String folder;
 }
