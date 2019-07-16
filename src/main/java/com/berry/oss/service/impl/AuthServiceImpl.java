544f93245f7434dd2d7629dd7faba92ebda3ba20
HiCooper <294237781@qq.com>
2019-07-16 22:42:41 +0800

    auth check
    Signed-off-by: HiCooper <294237781@qq.com>

--- src/main/java/com/berry/oss/service/impl/AuthServiceImpl.java
+++ src/main/java/com/berry/oss/service/impl/AuthServiceImpl.java
@@ -21,6 +21,8 @@ import java.util.List;
 @Service
 public class AuthServiceImpl implements IAuthService {
 
+    private static final String DENY = "DENY";
+
     private final IBucketService bucketService;
     private final IPolicyService policyService;
 
@@ -36,19 +38,42 @@ public class AuthServiceImpl implements IAuthService {
             // bucket 拥有者
             return true;
         }
-        // cooper/test/*
-        // cooper/tesat/a.jpg
         String targetResource = bucket + objectPath;
 
         // 获取该 bucket 授权策略
         List<PolicyListVo> policy = policyService.getPolicy(bucket);
-        return policy.stream().anyMatch(item -> {
+        boolean result = false;
+        for (PolicyListVo item : policy) {
+            // 遍历该用户相关的所有策略，DENY 优先级最高
             List<String> principal = item.getPrincipal();
             if (principal.stream().findAny().filter(name -> name.equals(user.getUsername())).orElse(null) != null) {
                 List<String> resource = item.getResource();
-                return checkHaveAccess(resource, targetResource);
+                String effect = item.getEffect();
+                if (effect.equals(DENY)) {
+                    if (!checkNotDeny(resource, targetResource)) {
+                        // 拒绝访问，直接终止判断，返回 false
+                        return false;
+                    }
+                } else {
+                    boolean access = checkHaveAccess(resource, targetResource);
+                    if (!result && access) {
+                        result = true;
+                    }
+                }
             }
-            return false;
+        }
+        return result;
+    }
+
+    private static boolean checkNotDeny(List<String> resource, String targetResource) {
+        // 任何一个 授权策略都不匹配 该目标对象，否则视为拒绝访问
+        return resource.stream().noneMatch(pattern -> {
+            if (pattern.endsWith("*")) {
+                pattern = pattern.replace("*", ".*");
+                return targetResource.matches(pattern);
+            } else {
+                return pattern.equals(targetResource);
+            }
         });
     }
 
@@ -65,6 +90,7 @@ public class AuthServiceImpl implements IAuthService {
      * @return true or false
      */
     private static boolean checkHaveAccess(List<String> resourcePatterns, String targetResource) {
+        // 任意一个 授权策略匹配 该目标对象即可，视为可访问
         return resourcePatterns.stream().anyMatch(pattern -> {
             if (pattern.endsWith("*")) {
                 pattern = pattern.replace("*", ".*");
