5b2030485b2ffafd12f80490327300b40d4ca58d
berry_cooper <berry_cooepr@163.com>
2019-08-23 13:01:11 +0800

    远程部署脚本 pass test

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+export LANG="en_US.UTF-8"
+
+#----------------------------------------------------------------------
+# 常量配置信息
+#----------------------------------------------------------------------
+
+# 远程服务器 ip
+remote_server_ip='10.50.12.38'
+username='root'
+# !确保远程文件夹 /home/jar-service 存在
+jar_store_dir='/home/jar-service/test'
+
+#----------------------------------------------------------------------
+# 脚本
+#----------------------------------------------------------------------
+
+# 构建项目
+echo "正在构建..."
+gradle build -x test
+
+if [ $? != 0 ]; then
+  echo "构建失败"
+  exit 1
+fi
+echo "构建成功"
+
+# 将jar 包 scp 值目标服务器指定目录下
+# shellcheck disable=SC2144
+if [ -f build/libs/*.jar ]; then
+  echo "正在上传 jar 包..."
+  cp run.sh build/libs/
+  scp -r build/libs/ ${username}@${remote_server_ip}:${jar_store_dir}
+  # shellcheck disable=SC2181
+  if [ $? != 0 ]; then
+    echo "上传失败"
+    exit 1
+  fi
+fi
+echo "上传成功"
+
+# ssh远程启动jar
+# shellcheck disable=SC2035
+ssh ${username}@${remote_server_ip} ${jar_store_dir}/run.sh restart *.jar
--- run.sh
+++ run.sh
@@ -1,89 +1,95 @@
 #!/usr/bin/env bash
+export LANG="en_US.UTF-8"
 
 functionName=$1
 
 APP_NAME=$2
 
-
 usage() {
-    echo "Usage: run.sh [start|stop|halt|restart|status]"
+  echo "Usage: run.sh [start|stop|halt|restart|status]"
+  exit 1
+}
+
+function checkParams() {
+  if [[ ! -n ${functionName} ]] || [[ ! -n ${APP_NAME} ]]; then
+    echo "ERROR!!! Please check the command, need like ./run.sh start app.jar"
     exit 1
+  fi
 }
 
-function checkParams(){
-    if [[ ! -n ${functionName} ]] || [[ ! -n ${APP_NAME}  ]]; then
-        echo "ERROR!!! Please check the command, need like ./run.sh start app.jar"
-        exit 1
-    fi
+function getPid() {
+  tpid=$(ps -ef | grep java | grep ${APP_NAME} | grep -v grep | grep -v kill | awk '{print $2}')
+  echo ${tpid}
 }
 
-function start(){
-    checkParams
-    count=`ps -ef |grep java|grep ${APP_NAME}|grep -v grep|wc -l`
-    if [[ ${count} != 0 ]];then
-        echo "progress is running... try restart?"
-    else
-        nohup java -jar ${APP_NAME} > /dev/null 2>&1 &
-        echo ">>> start successed PID=$! <<<"
-    fi
+function start() {
+  checkParams
+  tpid=$(getPid)
+  if [[ -n ${tpid} ]]; then
+    echo "progress is running... try restart?"
+  else
+    nohup java -jar ${APP_NAME} &
+    echo ">>> start successed PID=$! <<<"
+  fi
 }
 
-function stop(){
-    checkParams
-    tpid=`ps -ef|grep java |grep ${APP_NAME}|grep -v grep|grep -v kill|awk '{print $2}'`
-    if [[ -n ${tpid} ]]; then
-      echo 'Kill Process!'
-      kill -9 ${tpid}
-      echo 'Stop Success!'
-    else
-        echo "Not Running."
-    fi
+function stop() {
+  checkParams
+  tpid=$(getPid)
+  if [[ -n ${tpid} ]]; then
+    echo "Kill Process! PID:${tpid}"
+    kill -9 ${tpid}
+    echo 'Stop Success!'
+  else
+    echo "Not Running!"
+  fi
 }
 
-function halt(){
-    checkParams
-    tpid=`ps -ef|grep java |grep ${APP_NAME}|grep -v grep|grep -v kill|awk '{print $2}'`
-    if [[ -n ${tpid} ]]; then
-        echo 'Kill Process!'
-        kill -9 ${tpid}
-    fi
+function halt() {
+  checkParams
+  tpid=$(getPid)
+  if [[ -n ${tpid} ]]; then
+    echo "Kill Process! PID:${tpid}"
+    kill -9 ${tpid}
+  else
+    echo "Not Running!"
+  fi
 }
 
-function status(){
-    checkParams
-    count=`ps -ef |grep java  |grep ${APP_NAME}|grep -v grep|wc -l`
-    if [[ ${count} != 0 ]];then
-        echo "Running..."
-    else
-        echo "NOT running."
-    fi
+function status() {
+  checkParams
+  tpid=$(getPid)
+  if [[ -n ${tpid} ]]; then
+    echo "Running..."
+  else
+    echo "NOT running."
+  fi
 }
 
-function restart(){
-    checkParams
-    stop
-    sleep 2
-    start
+function restart() {
+  stop
+  sleep 2
+  start
 }
 
 case ${functionName} in
-    "start" )
-        start
-        ;;
-    "stop" )
-        stop
-        ;;
-    "halt" )
-        halt
-        ;;
-    "status" )
-        status
-        ;;
-    "restart" )
-        restart
-        ;;
-    * )
-        usage
-        ;;
+"start")
+  start
+  ;;
+"stop")
+  stop
+  ;;
+"halt")
+  halt
+  ;;
+"status")
+  status
+  ;;
+"restart")
+  restart
+  ;;
+*)
+  usage
+  ;;
 esac
 exit 0
