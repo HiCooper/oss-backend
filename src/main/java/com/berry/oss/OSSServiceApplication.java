35189d4c1a30d4c998d5ca7a76eda2b63841f43b
HiCooper <294237781@qq.com>
2019-12-23 10:14:17 +0800

    log

--- src/main/java/com/berry/oss/OSSServiceApplication.java
+++ src/main/java/com/berry/oss/OSSServiceApplication.java
@@ -1,14 +1,57 @@
 package com.berry.oss;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author xueancao
  */
 @SpringBootApplication
 public class OSSServiceApplication {
+    private static final Logger log = LoggerFactory.getLogger(OSSServiceApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(OSSServiceApplication.class, args);
+        ConfigurableEnvironment environment = SpringApplication.run(OSSServiceApplication.class, args).getEnvironment();
+        logApplicationStartup(environment);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
     }
 }
