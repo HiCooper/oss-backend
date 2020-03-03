7de2045bb0c636611b371a465b6486506c98d307
HiCooper <294237781@qq.com>
2020-03-03 17:58:46 +0800

    web config
    Signed-off-by: HiCooper <294237781@qq.com>

--- src/main/java/com/berry/oss/config/WebMvcConfig.java
+++ src/main/java/com/berry/oss/config/WebMvcConfig.java
@@ -1,20 +1,10 @@
 package com.berry.oss.config;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.berry.oss.security.interceptor.AccessInterceptor;
 import com.berry.oss.security.interceptor.AccessProvider;
-import org.apache.http.Consts;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -31,53 +21,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public WebMvcConfig(AccessProvider accessProvider) {
         this.accessProvider = accessProvider;
-    }
-
-    /**
-     * 将返回值序列化，如数字型null输出0，字符串类型null输出 空字符串
-     *
-     * @param converters
-     */
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(fastJsonHttpMessageConverter());
-    }
-
-    @Bean
-    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
-        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        FastJsonConfig config = new FastJsonConfig();
-        List<MediaType> supportedMediaTypes = new ArrayList<>();
-        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
-        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
-        supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
-        supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
-        supportedMediaTypes.add(MediaType.APPLICATION_PDF);
-        supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
-        supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
-        supportedMediaTypes.add(MediaType.APPLICATION_XML);
-        supportedMediaTypes.add(MediaType.IMAGE_GIF);
-        supportedMediaTypes.add(MediaType.IMAGE_JPEG);
-        supportedMediaTypes.add(MediaType.IMAGE_PNG);
-        supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
-        supportedMediaTypes.add(MediaType.TEXT_HTML);
-        supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
-        supportedMediaTypes.add(MediaType.TEXT_PLAIN);
-        supportedMediaTypes.add(MediaType.TEXT_XML);
-        converter.setSupportedMediaTypes(supportedMediaTypes);
-        config.setSerializerFeatures(
-                // 禁用循环引用检测
-                SerializerFeature.DisableCircularReferenceDetect,
-                SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteNullStringAsEmpty,
-                SerializerFeature.WriteNullBooleanAsFalse,
-                SerializerFeature.WriteNullListAsEmpty,
-                //输出时间格式化
-                SerializerFeature.WriteDateUseDateFormat);
-        converter.setFastJsonConfig(config);
-        converter.setDefaultCharset(Consts.UTF_8);
-        return converter;
     }
 
     /**
