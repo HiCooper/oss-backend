ad30f77eeb670dfbd2bbc240b33c5b59436f5063
HiCooper <294237781@qq.com>
2020-04-11 12:50:12 +0800

    check change
    Signed-off-by: HiCooper <294237781@qq.com>

--- src/main/java/com/berry/oss/security/dao/entity/UserRole.java
+++ src/main/java/com/berry/oss/security/dao/entity/UserRole.java
@@ -1,5 +1,7 @@
 package com.berry.oss.security.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
@@ -26,6 +28,7 @@ public class UserRole implements Serializable {
     /**
      * 主键
      */
+    @TableId(type = IdType.AUTO)
     private Integer id;
 
     /**
