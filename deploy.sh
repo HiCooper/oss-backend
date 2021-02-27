a9cbdcb5d657dde02229d7530ea0a1de9d44788e
HiCooper <caoxuean@gmail.com>
2021-02-27 14:20:59 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,14 @@ export LANG="en_US.UTF-8"
 # 远程服务器 ip
 #remote_server_ip='47.101.42.169'
 #remote_server_ip='10.50.5.28'
-remote_server_ip='10.0.102.250'
-username='centos'
+#remote_server_ip='10.0.102.250'
+remote_server_ip='192.168.33.10'
+username='vagrant'
 # !确保远程文件夹存在
 #jar_store_dir='/home/jar-task/backend-service'
 #jar_store_dir='/home/sjtu-dev/pd-micro-service/oss-service'
-jar_store_dir='/home/centos/jar-project/oss-backend'
+#jar_store_dir='/home/centos/jar-project/oss-backend'
+jar_store_dir='/home/vagrant/jar-project/oss'
 
 #----------------------------------------------------------------------
 # 脚本
@@ -21,11 +23,11 @@ jar_store_dir='/home/centos/jar-project/oss-backend'
 
 # 构建项目
 echo "正在构建..."
-gradle build -x test
+gradle clean build -x test
 
 if [ $? != 0 ]; then
   echo "构建失败"
-  exit 1
+  read
 fi
 echo "构建成功"
 
@@ -33,12 +35,12 @@ echo "构建成功"
 # shellcheck disable=SC2144
 if [ -f ./build/libs/*.jar ]; then
   echo "正在上传 jar 包..."
-#  cp run.sh build/libs/
-  scp ./build/libs/*.jar ${username}@${remote_server_ip}:${jar_store_dir}
+  cp run.sh build/libs/
+  scp build/libs/* ${username}@${remote_server_ip}:${jar_store_dir}
   # shellcheck disable=SC2181
   if [[ $? != 0 ]]; then
     echo "上传失败"
-    exit 1
+    read
   fi
   echo "上传成功"
 fi
